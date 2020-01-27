acd11e6ca0e2aae19103393812516c3336696088
Raine <akita.neru@gmx.de>
2020-01-27 15:47:53 +0100

    Item Prefabs
    Added Item Prefabs

---
+++ BugKartMMO/Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Network;
+
+public class GameManager : NetworkBehaviour
+{
+    // Bool = is finished
+    // for each player, check if finished
+    // if isfinished == true --> game stop // scene change to highscore or Win List
+    // Player send back to lobby
+
+    // if Player disconnects == game stop --> back to lobby
+    // 
+
+
+    protected override void Update()
+    {
+
+    }
+}

ac8aa89c66d6ee5f27d644a9daa8ff3e83b57467
Raine <akita.neru@gmx.de>
2020-01-30 17:34:48 +0100

    StartPosition
    Update Start Postion

--- BugKartMMO/Assets/Scripts/Game/StartPosition.cs
+++ BugKartMMO/Assets/Scripts/Game/StartPosition.cs
@@ -5,7 +5,9 @@ public class StartPosition : MonoBehaviour
     [SerializeField]
     private GameObject m_Player;
     [SerializeField]
-    private Transform[] m_StartPoint;
+    private Transform[] m_StartPoint = new Transform[5];
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -16,23 +18,49 @@ public class StartPosition : MonoBehaviour
             case 0:
                 Debug.Log("Player One");
                 // Instantiate(m_Player, Hier Gewünschte Position eingeben, transform.parent.rotation);
-                Instantiate(m_Player);
+                Instantiate(m_Player, m_StartPoint[0].position, transform.parent.rotation);
+
+                // change Color of Player
+                Renderer rendP1 = m_Player.GetComponent<Renderer>();
+                rendP1.material.color = Color.blue;
                 break;
             case 1:
                 Debug.Log("Player Two");
                 // Instantiate(m_Player, Hier Gewünschte Position eingeben, transform.parent.rotation);
+                Instantiate(m_Player, m_StartPoint[1].position, transform.parent.rotation);
+
+                // change Color of Player
+                Renderer rendP2 = m_Player.GetComponent<Renderer>();
+                rendP2.material.color = Color.red;
                 break;
             case 2:
                 Debug.Log("Player Three");
                 // Instantiate(m_Player, Hier Gewünschte Position eingeben, transform.parent.rotation);
+                Instantiate(m_Player, m_StartPoint[2].position, transform.parent.rotation);
+
+                // change Color of Player
+                Renderer rendP3 = m_Player.GetComponent<Renderer>();
+                rendP3.material.color = Color.green;
                 break;
             case 3:
                 Debug.Log("Player Four");
                 // Instantiate(m_Player, Hier Gewünschte Position eingeben, transform.parent.rotation);
+                Instantiate(m_Player, m_StartPoint[3].position, transform.parent.rotation);
+
+
+                // change Color of Player
+                Renderer rendP4 = m_Player.GetComponent<Renderer>();
+                rendP4.material.color = Color.magenta;
                 break;
             case 4:
                 Debug.Log("Player Five");
                 // Instantiate(m_Player, Hier Gewünschte Position eingeben, transform.parent.rotation);
+                Instantiate(m_Player, m_StartPoint[4].position, transform.parent.rotation);
+
+
+                // change Color of Player
+                Renderer rendP5 = m_Player.GetComponent<Renderer>();
+                rendP5.material.color = Color.gray;
                 break;
         }
     }
